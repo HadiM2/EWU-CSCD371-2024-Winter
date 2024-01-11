e1819a0b5dd91b1b1885d267f0298eac6a919da2
Hadi <amallah@ewu.edu>
2024-01-11 15:39:18 -0800

    Update 2

--- ClassDemo/ClassDemo/Program.cs
+++ ClassDemo/ClassDemo/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace ClassDemo;
 
-internal class Program
+class Program //internal class Program: Internal is an acess modifier, means you can only acess with this module
 {
     static void Main(string[] args)
     {
+        // F12 to go to method, Ctrl to "go back"
+        Console.WriteLine()
         Console.WriteLine("Hello, World!");
         console.WriteLine("Meow");
+        // Everything is an object and everything is a type in C#
+        1.toString();
+        "1".getType();
+        Console.WriteLine(1.GetType().ToString())
     }
 }
